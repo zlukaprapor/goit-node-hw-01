972517419a0423b8ddc99a697b29cfa0d4294962
Oleksii Tkachenko <zlukapraporr@gmail.com>
2022-03-31 23:54:50 +0300

    start

---
+++ contacts.js
@@ -0,0 +1,25 @@
+// contacts.js
+const fs = require('fs/promises');
+const path = require('path');
+
+/*
+ * Раскомментируй и запиши значение
+ * const contactsPath = ;
+ */
+
+// TODO: задокументировать каждую функцию
+function listContacts() {
+  // ...твой код
+}
+
+function getContactById(contactId) {
+  // ...твой код
+}
+
+function removeContact(contactId) {
+  // ...твой код
+}
+
+function addContact(name, email, phone) {
+  // ...твой код
+}
