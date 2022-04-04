e94f4e0213cd3c1ee6bf87c8633b503d29192479
Oleksii Tkachenko <zlukapraporr@gmail.com>
2022-04-04 22:31:20 +0300

    add action list

--- index.js
+++ index.js
@@ -1,3 +1,41 @@
+const { Command } = require('commander');
+const chalk = require('chalk');
 const { listContacts } = require('./contacts');
 
-console.log(listContacts());
+const program = new Command();
+program
+  .option('-a, --action <type>', 'choose action')
+  .option('-i, --id <type>', 'user id')
+  .option('-n, --name <type>', 'user name')
+  .option('-e, --email <type>', 'user email')
+  .option('-p, --phone <type>', 'user phone');
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+// TODO: рефакторить
+function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case 'list':
+      listContacts().then((contacts) => console.table(contacts));
+      break;
+
+    case 'get':
+      // ... id
+      break;
+
+    case 'add':
+      // ... name email phone
+      break;
+
+    case 'remove':
+      // ... id
+      break;
+
+    default:
+      console.warn('\x1B[31m Unknown action type!');
+  }
+}
+
+invokeAction(argv);
